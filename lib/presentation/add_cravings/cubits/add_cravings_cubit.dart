e78a76c3ab517fa7c7ccd3a5b7732b115215c712
Tenten Ponce <poncetenten10@gmail.com>
2024-08-17 21:13:59 +0800

    Added analytics

--- lib/presentation/add_cravings/cubits/add_cravings_cubit.dart
+++ lib/presentation/add_cravings/cubits/add_cravings_cubit.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:injectable/injectable.dart';
+import 'package:kycravings/core/infrastructure/constants/analytics_events.dart';
+import 'package:kycravings/core/infrastructure/platform/firebase_app_analytics.dart';
 import 'package:kycravings/data/db/repositories/categories_repository.dart';
 import 'package:kycravings/data/db/repositories/cravings_repository.dart';
 import 'package:kycravings/domain/models/category_model.dart';
@@ -12,14 +16,17 @@ class AddCravingsCubit extends BaseCubit<AddCravingsState> {
   final CravingsRepository _cravingsRepository;
   final CategoriesRepository _categoriesRepository;
   final DebouncerUtils _debouncerUtils;
+  final FirebaseAppAnalytics _firebaseAppAnalytics;
   AddCravingsCubit(
     this._cravingsRepository,
     this._categoriesRepository,
     this._debouncerUtils,
+    this._firebaseAppAnalytics,
   ) : super(const AddCravingsState.on());
 
   @override
   Future<void> init() async {
+    unawaited(_firebaseAppAnalytics.logEvent(name: AnalyticsEvents.eventOpenAddCraving));
     _debouncerUtils.setMilliseconds(500);
 
     final categories = await _categoriesRepository.selectAll();
@@ -28,10 +35,18 @@ class AddCravingsCubit extends BaseCubit<AddCravingsState> {
 
   Future<bool> addCraving(String cravingName) async {
     if (!await _isValidCraving(cravingName)) {
+      unawaited(_firebaseAppAnalytics.logEvent(
+        name: AnalyticsEvents.eventAddCravingFail,
+        parameters: {AnalyticsEvents.paramError: state.cravingError.toString()},
+      ));
       return false;
     }
 
     await _cravingsRepository.insert(cravingName, state.categories.where((category) => category.isSelected ?? false));
+    unawaited(_firebaseAppAnalytics.logEvent(
+      name: AnalyticsEvents.eventAddCravingSuccess,
+      parameters: {AnalyticsEvents.paramCraving: cravingName},
+    ));
     return true;
   }
 
@@ -48,20 +63,39 @@ class AddCravingsCubit extends BaseCubit<AddCravingsState> {
         return category;
       }
     }).toList()));
+
+    final clickedCategory = state.categories.firstWhere((category) => category.id == categoryId);
+
+    unawaited(_firebaseAppAnalytics.logEvent(
+      name: AnalyticsEvents.eventToggleCategory,
+      parameters: {
+        AnalyticsEvents.paramCategory: clickedCategory.name,
+        AnalyticsEvents.paramToggle: clickedCategory.isSelected.toString(),
+      },
+    ));
   }
 
   Future<bool> addCategory(String categoryName) async {
     if (!_isValidCategory(categoryName)) {
+      unawaited(_firebaseAppAnalytics.logEvent(
+        name: AnalyticsEvents.eventAddCategoryFail,
+        parameters: {AnalyticsEvents.paramError: state.categoryError.toString()},
+      ));
       return false;
     }
 
     final category = await _categoriesRepository.insert(categoryName);
     emit(state.copyWith(categories: state.categories.toList()..add(category.copyWith(isSelected: true))));
+    unawaited(_firebaseAppAnalytics.logEvent(
+      name: AnalyticsEvents.eventAddCategorySuccess,
+      parameters: {AnalyticsEvents.paramCraving: categoryName},
+    ));
     return true;
   }
 
   void onAddCategory() {
     emit(state.copyWith(categoryError: CategoryError.none));
+    unawaited(_firebaseAppAnalytics.logEvent(name: AnalyticsEvents.eventOpenAddCategory));
   }
 
   void onCategoryChanged(String categoryName) {
@@ -70,6 +104,11 @@ class AddCravingsCubit extends BaseCubit<AddCravingsState> {
 
   void onLongPressCategory(int categoryId) {
     _categoriesRepository.remove(categoryId);
+    final removedCategory = state.categories.firstWhere((category) => category.id == categoryId);
+    unawaited(_firebaseAppAnalytics.logEvent(
+      name: AnalyticsEvents.eventRemoveCategory,
+      parameters: {AnalyticsEvents.paramCraving: removedCategory.name},
+    ));
     emit(state.copyWith(categories: state.categories.where((category) => category.id != categoryId).toList()));
   }
 
--- test/presentation/your_cravings/your_cravings_cubit_test.dart
+++ test/presentation/your_cravings/your_cravings_cubit_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:kycravings/core/infrastructure/platform/firebase_app_analytics.dart';
 import 'package:kycravings/data/db/repositories/cravings_repository.dart';
 import 'package:kycravings/domain/models/craving_model.dart';
 import 'package:kycravings/presentation/your_cravings/cubits/your_cravings_cubit.dart';
@@ -9,19 +10,23 @@ import 'your_cravings_cubit_test.mocks.dart';
 
 @GenerateNiceMocks([
   MockSpec<CravingsRepository>(),
+  MockSpec<FirebaseAppAnalytics>(),
 ])
 void main() {
   group(YourCravingsCubit, () {
     late MockCravingsRepository mockCravingsRepository;
+    late MockFirebaseAppAnalytics mockFirebaseAppAnalytics;
 
     setUp(() {
       mockCravingsRepository = MockCravingsRepository();
+      mockFirebaseAppAnalytics = MockFirebaseAppAnalytics();
     });
 
     YourCravingsCubit createUnitToTest() {
       return YourCravingsCubit(
         mockCravingsRepository,
-      );
+        mockFirebaseAppAnalytics,
+      )..onInitialLoad = () {};
     }
 
     test('getCravings should emit cravings successfully', () async {
@@ -29,13 +34,83 @@ void main() {
         CravingModel.test,
         CravingModel.test,
       ];
-      when(mockCravingsRepository.selectWithCategories()).thenAnswer((_) async => mockCravings);
+      when(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).thenAnswer((_) async => mockCravings);
 
       final unit = createUnitToTest();
       await unit.getCravings();
 
-      verify(mockCravingsRepository.selectWithCategories()).called(2);
+      verify(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).called(2);
       expect(unit.state.cravings, mockCravings);
+    });
+
+    test('onScrollBottom should append new list if has more data', () async {
+      final mockCravings = [
+        CravingModel.test,
+        CravingModel.test,
+      ];
+
+      when(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).thenAnswer((_) async => mockCravings);
+
+      final unit = createUnitToTest();
+
+      await unit.init();
+
+      final newCravings = [
+        CravingModel.test.copyWith(id: 3),
+        CravingModel.test.copyWith(id: 4),
+      ];
+
+      when(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).thenAnswer((_) async => newCravings);
+
+      await unit.onScrollBottom();
+
+      verify(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).called(3);
+
+      expect(unit.state.cravings, [...mockCravings, ...newCravings]);
+    });
+
+    test('onScrollBottom should not call craving history if already reached bottom', () async {
+      final mockCravingHistory = [
+        CravingModel.test,
+        CravingModel.test,
+      ];
+
+      when(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).thenAnswer((_) async => mockCravingHistory);
+
+      final unit = createUnitToTest();
+
+      await unit.init();
+
+      when(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).thenAnswer((_) async => []);
+
+      await unit.onScrollBottom();
+      await unit.onScrollBottom();
+
+      verify(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).called(3);
     });
 
     test('onCravingDelete should delete craving and remove from state', () async {
@@ -44,12 +119,15 @@ void main() {
         CravingModel.test.copyWith(id: 2),
       ];
       const cravingIdToDelete = 2;
-      when(mockCravingsRepository.selectWithCategories()).thenAnswer((_) async => mockCravings);
+      when(mockCravingsRepository.selectWithCategories(
+        limit: anyNamed('limit'),
+        offset: anyNamed('offset'),
+      )).thenAnswer((_) async => mockCravings);
 
       final unit = createUnitToTest();
       await unit.getCravings();
 
-      unit.onCravingDelete(cravingIdToDelete);
+      await unit.onCravingDelete(cravingIdToDelete);
 
       verify(mockCravingsRepository.remove(cravingIdToDelete)).called(1);
       expect(unit.state.cravings, mockCravings..remove(mockCravings[1]));
