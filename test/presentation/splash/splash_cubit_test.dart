48ece408bc7fc8938569a2debdc8e186c7ded0ae
Tenten Ponce <poncetenten10@gmail.com>
2024-08-18 18:39:19 +0800

    test

--- test/presentation/splash/splash_cubit_test.dart
+++ test/presentation/splash/splash_cubit_test.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:kycravings/core/logging/logger.dart';
+import 'package:kycravings/presentation/shared/utils/navigation_utils.dart';
 import 'package:kycravings/presentation/update_cravings/cubits/update_cravings_cubit.dart';
 import 'package:mockito/annotations.dart';
 
 @GenerateNiceMocks([
   MockSpec<Logger>(),
+  MockSpec<NavigationUtils>(),
 ])
 void main() {
   group(UpdateCravingsCubit, () {
