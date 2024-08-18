9f0b17b18bb53a2fdb62d55ddd11921bb876252b
Tenten Ponce <poncetenten10@gmail.com>
2024-08-18 18:45:46 +0800

    Added navigation mapper

--- lib/presentation/shared/utils/navigation_utils.dart
+++ lib/presentation/shared/utils/navigation_utils.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:injectable/injectable.dart';
-import 'package:kycravings/presentation/home/home_view.dart';
+import 'package:kycravings/presentation/shared/utils/navigation_mapper.dart';
 
 enum ViewRoute {
   home,
@@ -14,31 +14,28 @@ abstract interface class NavigationUtils {
 @LazySingleton(as: NavigationUtils)
 class NavigationUtilsImpl implements NavigationUtils {
   final GlobalKey<NavigatorState> navigatorKey;
+  final NavigationMapper _navigationMapper;
 
-  NavigationUtilsImpl(this.navigatorKey);
+  NavigationUtilsImpl(
+    this.navigatorKey,
+    this._navigationMapper,
+  );
 
   @override
   Future<T?>? push<T>(ViewRoute viewRoute, {Object? arguments}) {
     return navigatorKey.currentState?.push(
       MaterialPageRoute<T>(
-        builder: (context) => _getRoute(viewRoute),
+        builder: (context) => _navigationMapper.getRoute(viewRoute),
         settings: RouteSettings(arguments: arguments),
       ),
     );
-  }
-
-  Widget _getRoute(ViewRoute viewRoute) {
-    switch (viewRoute) {
-      case ViewRoute.home:
-        return HomeView();
-    }
   }
 
   @override
   Future<T?>? pushReplacement<T>(ViewRoute viewRoute, {Object? arguments}) {
     return navigatorKey.currentState?.pushReplacement(
       MaterialPageRoute<T>(
-        builder: (context) => _getRoute(viewRoute),
+        builder: (context) => _navigationMapper.getRoute(viewRoute),
         settings: RouteSettings(arguments: arguments),
       ),
     );
